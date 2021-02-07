3d05e649115e62cc4da8d834c9056d74ca2a8272
wang42449 <63088118+wang42449@users.noreply.github.com>
2021-02-07 18:53:45 +0800

    Delete ing/mine.js directory

--- ing/mine.js/server/src/lib/server/prisma.js
+++
@@ -1,9 +0,0 @@
-import { Prisma } from 'prisma-binding'
-
-const prisma = new Prisma({
-  typeDefs: 'server/src/generated/prisma.graphql',
-  endpoint: 'http://localhost:4466',
-  secret: 'thisismysupersecrettext'
-})
-
-export default prisma
